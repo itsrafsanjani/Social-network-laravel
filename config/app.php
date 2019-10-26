152349c55aab68c3a9d56c2b661b6a4d3d8e8a68
OussamaZouaine <oussamazouaine@gmail.com>
2019-10-26 18:24:47 +0100

    hello

--- config/app.php
+++ config/app.php
@@ -199,6 +199,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        // hello
         // App\Providers\TelescopeServiceProvider::class,
         App\Providers\HelperServiceProvider::class,
         App\Providers\ObserverServiceProvider::class,
