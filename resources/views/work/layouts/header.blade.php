34f2d9957b1a08175b5ebfca45bcd660a3f37c96
adnaz <adnanaitzait@gmail.com>
2019-10-27 21:59:30 +0100

    info  done

--- resources/views/work/layouts/header.blade.php
+++ resources/views/work/layouts/header.blade.php
@@ -1,26 +1,42 @@
 @include('work.layouts.app')
 
-  <header>
+<header>
     <div class="container">
         <div class="header-data">
             <div class="logo">
-                <a href="index.html" title=""><img src="images/logo.png" alt=""></a>
-            </div><!--logo end-->
+                <a href="{{ route('frontend.index') }}" title="{{ app_name() }}"><img src="images/logo.png"
+                        alt="">
+                    {{-- <a href="{{ route('frontend.index') }}" class="navbar-brand">{{ app_name() }}</a> --}}
+                </a>
+            </div>
+            <!--logo end-->
+
             <div class="search-bar">
                 <form>
                     <input type="text" name="search" placeholder="Search...">
                     <button type="submit"><i class="la la-search"></i></button>
                 </form>
-            </div><!--search-bar end-->
+            </div>
+            <!--search-bar end-->
             <nav>
                 <ul>
+                    @if(config('locale.status') && count(config('locale.languages')) > 1)
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownLanguageLink"
+                            data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">@lang('menus.language-picker.language')
+                            ({{ strtoupper(app()->getLocale()) }})</a>
+
+                        @include('includes.partials.lang')
+                    </li>
+                    @endif
                     <li>
-                        <a href="index.html" title="">
+                        <a href="{{ route('frontend.index') }}" title="">
                             <span><img src="images/icon1.png" alt=""></span>
                             Home
                         </a>
                     </li>
-                    <li>
+                    {{-- <li>
                         <a href="companies.html" title="">
                             <span><img src="images/icon2.png" alt=""></span>
                             Companies
@@ -29,7 +45,7 @@
                             <li><a href="companies.html" title="">Companies</a></li>
                             <li><a href="company-profile.html" title="">Company Profile</a></li>
                         </ul>
-                    </li>
+                    </li> --}}
                     <li>
                         <a href="projects.html" title="">
                             <span><img src="images/icon3.png" alt=""></span>
@@ -39,7 +55,7 @@
                     <li>
                         <a href="profiles.html" title="">
                             <span><img src="images/icon4.png" alt=""></span>
-                            Profiles
+                            Requests
                         </a>
                         <ul>
                             <li><a href="user-profile.html" title="">User Profile</a></li>
@@ -64,40 +80,46 @@
                             </div>
                             <div class="nott-list">
                                 <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img1.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="messages.html" title="">Jassica William</a> </h3>
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img2.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="messages.html" title="">Jassica William</a></h3>
-                                          <p>Lorem ipsum dolor sit amet.</p>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img3.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="messages.html" title="">Jassica William</a></h3>
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua.</p>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="view-all-nots">
-                                      <a href="messages.html" title="">View All Messsages</a>
-                                  </div>
-                            </div><!--nott-list end-->
-                        </div><!--notification-box end-->
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img1.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="messages.html" title="">Jassica William</a> </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img2.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="messages.html" title="">Jassica William</a></h3>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img3.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="messages.html" title="">Jassica William</a></h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                            eiusmod tempo incididunt ut labore et dolore magna aliqua.</p>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="view-all-nots">
+                                    <a href="messages.html" title="">View All Messsages</a>
+                                </div>
+                            </div>
+                            <!--nott-list end-->
+                        </div>
+                        <!--notification-box end-->
                     </li>
                     <li>
                         <a href="#" title="" class="not-box-open">
@@ -111,52 +133,64 @@
                             </div>
                             <div class="nott-list">
                                 <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img1.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img2.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img3.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-                                          <span>2 min ago</span>
-                                      </div><!--notification-info -->
-                                  </div>
-                                  <div class="notfication-details">
-                                      <div class="noty-user-img">
-                                          <img src="images/resources/ny-img2.png" alt="">
-                                      </div>
-                                      <div class="notification-info">
-                                          <h3><a href="#" title="">Jassica William</a> Comment on your project.</h3>
-                                          <span>2 min ago</span>
-                                        </div>
-                                    </div><!--notification-info -->
-                                  <div class="view-all-nots">
-                                      <a href="#" title="">View All Notification</a>
-                                  </div>
-                            </div><!--nott-list end-->
-                        </div><!--notification-box end-->
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img1.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img2.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img3.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img2.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div>
+                                    <!--notification-info -->
+                                </div>
+                                <div class="view-all-nots">
+                                    <a href="#" title="">View All Notification</a>
+                                </div>
+                            </div>
+                            <!--nott-list end-->
+                        </div>
+                        <!--notification-box end-->
                     </li>
                 </ul>
-            </nav><!--nav end-->
+            </nav>
+            <!--nav end-->
             <div class="menu-btn">
                 <a href="#" title=""><i class="fa fa-bars"></i></a>
-            </div><!--menu-btn end-->
+            </div>
+            <!--menu-btn end-->
             <div class="user-account">
                 <div class="user-info">
                     <img src="http://via.placeholder.com/30x30" alt="">
@@ -191,19 +225,30 @@
                             <input type="text" name="search">
                             <button type="submit">Ok</button>
                         </form>
-                    </div><!--search_form end-->
+                    </div>
+                    <!--search_form end-->
                     <h3>Setting</h3>
                     <ul class="us-links">
-                        <li><a href="profile-account-setting.html" title="">Account Setting</a></li>
+                        <li> 
+                                @can('view backend')
+                                    <a href="{{ route('admin.dashboard') }}" >@lang('navs.frontend.user.administration')</a>
+                                @endcan
+                        </li>		
+                            <li><a href="{{ route('frontend.user.account') }}" {{ active_class(Route::is('frontend.user.account')) }}>@lang('navs.frontend.user.account')</a></li>
+                        
                         <li><a href="#" title="">Privacy</a></li>
                         <li><a href="#" title="">Faqs</a></li>
                         <li><a href="#" title="">Terms & Conditions</a></li>
                     </ul>
-                    <h3 class="tc"><a href="sign-in.html" title="">Logout</a></h3>
-                </div><!--user-account-settingss end-->
+                    <h3 class="tc"> <a href="{{ route('frontend.auth.logout') }}"
+                            class="dropdown-item">@lang('navs.general.logout')</a>
+                    </h3>
+                </div>
+                <!--user-account-settingss end-->
             </div>
-        </div><!--header-data end-->
+        </div>
+        <!--header-data end-->
     </div>
-</header><!--header end-->
-
+</header>
+<!--header end-->
 @yield('contents')
